2c4051ea584400586a3127bde076d213ff52a7a0
Declan Davis <dexthefish@gmail.com>
2022-08-05 13:01:26 -0400

    add user seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -2,12 +2,26 @@ INSERT INTO users
 (name, treats, password, profile_picture, bio, age, address) VALUES
 ('Dex', 7, '123', 'https://helloartsy.com/wp-content/uploads/kids/cats/easy_cat_face_drawing/easy-cat-face-drawing_step-6.jpg', 'I love getting my paws dirty', 26, '111 example road, Toronto');
 INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
-('Mia', 1, '123', 'photo url', 'only here to gain instagram followers', 18, '111 example road, Toronto');
+('Mia', 1, '123', 'https://www.litter-robot.com/media/magefan_blog/2018/10/korat-352x510.jpg', 'only here to gain instagram followers', 18, '111 example road, Toronto');
 INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
-('Joey', 10, '123', 'photo url', 'i enjoy long walks and catnip', 47, '111 example road, Toronto');
+('Joey', 10, '123', 'https://petfocus.ca/dartmouth/wp-content/uploads/sites/43/2020/01/black-cat.jpg', 'i enjoy long walks and catnip', 47, '111 example road, Toronto');
 INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
-('Clifford', 5, '123', 'photo url', 'frisbee! frisbee! arf arf', 2, '111 example road, Toronto');
+('Clifford', 5, '123', 'https://rochester.kidsoutandabout.com/sites/default/files/styles/970w/public/Clifford%201.jpeg?itok=CD_uywh1&sc=555562867d6dacab2b4b1b90ba69c32f', 'frisbee! frisbee! arf arf', 2, '111 example road, Toronto');
 INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
-('Garfield', 15, '123', 'photo url', 'if you make good lasagne then I will never leave you', 36, '111 example road, Toronto');
+('Garfield', 15, '123', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Garfield_the_Cat.svg/1200px-Garfield_the_Cat.svg.png', 'if you make good lasagne then I will never leave you', 36, '111 example road, Toronto');
 INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
-('Grumpy Cat', 0, '123', 'photo url', 'y u no swipe me', 83, '111 example road, Toronto');
+('Grumpy Cat', 0, '123', 'https://i.kym-cdn.com/entries/icons/facebook/000/011/365/GRUMPYCAT.jpg', 'y u no swipe me', 83, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Crookshanks', 10, '123', 'https://static.wikia.nocookie.net/harrypotter/images/f/ff/Crookshanks_FH.png/revision/latest?cb=20161118055031', 'Slightly squashed face, but with a heart of gold. Meow. I usually hang out with Hermione so pls dont swipe unless you like humans. Mrrrreeow. I like dogs too', 51, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Cheshire', 12, '123', 'photo url', 'probably mad as a hatter. If you dont know where you are going, any road will take you there.', 67, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Hobbes', 18, '123', 'photo url', 'I am a real tiger. Rawr. Not a stuffed toy. I like orange stripes. Come for the tail, stay for the philosophical conversations.', 12, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Mr. Whiskers', 5, '123', 'photo url', 'A proper gentlecat. If you are classy like me then we will get along great. My favourite hobby is hissing at stray cats. No dogs please.', 52, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Brian', 9, '123', 'https://i.ytimg.com/vi/GFAQ4cbyDsM/maxresdefault.jpg', 'Aspiring writer. Interested in humans or dogs. Looking for someone smart... but not too smart.', 18, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Toto', 1, '123', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk1DzLRXEeD6gWfKFYbND6PHqWUN1hmIcZ-A&usqp=CAU', 'woof! I like belly rubs and playing at the park. I have a soft spot for golden retrievers.', 33, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('CoCo', 1, '123', 'photo url', 'bark bark, woof woof, chase the squirrel, eat the treats', 29, '111 example road, Toronto');
