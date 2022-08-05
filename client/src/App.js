07572083e645f84d12b720ce1992cd55e9f325df
Declan Davis <dexthefish@gmail.com>
2022-08-05 14:20:05 -0400

    experiment with axios and useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 import PurpleCat from './lotties/PurpleCat';
 
 function App() {
+  const [state, setState] = useState('default state');
+
+  useEffect(() => {
+    Promise.all(
+      [axios.get('/users')]
+      ).then((all) => {
+      setState(prev => all[0].data); //grab data with all[0].data
+      console.log('after the axios request:  ', state);
+    })
+  }, [])
+
+
+
   return (
     <div className="App">
       <PurpleCat />
--- server/routes/users.js
+++ server/routes/users.js
@@ -12,5 +12,41 @@ module.exports = (db) => {
     })
   });
 
+  router.get('/:id', (req, res) => {
+    // get profile info for a given user
+    const queryString = `SELECT (name, treats, profile_picture, bio, age, address) FROM users WHERE id=$1`;
+    const queryParams = [ req.params.id ]
+    db.query(queryString, queryParams).then(data => {
+      res.json(data.rows[0]);
+    })
+  });
+
+
+  router.post('/', (req, res) => {
+    // allow user to create new profile
+    // grab new-user data from req.body
+    // command = INSERT INTO users VALUES (... req.body)
+    // db.query(command).then(() => {} )
+  })
+
+  
+  router.put('/:id', (req, res) => {
+    // allow user to edit their own profile
+    const { name, bio, age, address } = req.body
+
+    const queryString = ` 
+    UPDATE users
+    SET name = $1, bio = $2, age = $3, address = $4
+    WHERE id = $5
+    `;
+
+    const queryParams = [name, bio, age, address, req.params.id]
+    
+    db.query(queryString, queryParams).then(data => {
+      res.json(data.rows)
+    })
+  
+    
+  })
   return router;
 }
