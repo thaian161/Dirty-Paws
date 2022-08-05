cc0e48824bc6f723ed41655d8f761f5b13f62b91
thaian161 <thaian161@yahoo.com>
2022-08-05 16:38:58 -0500

    remove reportWebVital

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import ChatBubble from './lotties/ChatBubble';
 import PurpleCat from './lotties/PurpleCat';
 import RainbowCat from './lotties/RainbowCat';
 
-import ProfileCard from './components/profile/Profile';
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <RainbowCat />
       <ChatBubble />
 
-      <ProfileCard />
+     
     </div>
   );
 }
