97127971aa754f1d554b327507adc5a698a2418e
Declan Davis <dexthefish@gmail.com>
2022-08-05 11:31:37 -0400

    add user seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,3 +1,13 @@
 INSERT INTO users 
 (name, treats, password, profile_picture, bio, age, address) VALUES
 ('Dex', 7, '123', 'https://helloartsy.com/wp-content/uploads/kids/cats/easy_cat_face_drawing/easy-cat-face-drawing_step-6.jpg', 'I love getting my paws dirty', 26, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Mia', 1, '123', 'photo url', 'only here to gain instagram followers', 18, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Joey', 10, '123', 'photo url', 'i enjoy long walks and catnip', 47, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Clifford', 5, '123', 'photo url', 'frisbee! frisbee! arf arf', 2, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Garfield', 15, '123', 'photo url', 'if you make good lasagne then I will never leave you', 36, '111 example road, Toronto');
+INSERT INTO users (name, treats, password, profile_picture, bio, age, address) VALUES
+('Grumpy Cat', 0, '123', 'photo url', 'y u no swipe me', 83, '111 example road, Toronto');
