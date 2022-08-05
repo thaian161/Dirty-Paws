7411396fa52b746915985519e312f9f56dcc5006
Declan Davis <dexthefish@gmail.com>
2022-08-05 11:13:44 -0400

    add profile view with 'swipe' button

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 // const users = ['Ann', 'Dan', 'Dex', 'Mia'];
 
 module.exports = (db) => {
-  // all routes will go here 
+  // all /users routes will go here 
   router.get('/', (req, res) => {
     const command = 'SELECT * FROM users';
 
     db.query(command).then(data => {
-      res.json(data.rows);
+      res.json(data.rows[Math.floor(Math.random() * data.rows.length)]);
     })
   });
 
