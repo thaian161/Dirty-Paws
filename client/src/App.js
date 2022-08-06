7b8e3456d4d05835d076d96f6d84e8f031036a33
thaian161 <thaian161@yahoo.com>
2022-08-05 23:46:42 -0500

    add Nav component and styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import RainbowCat from './lotties/RainbowCat';
 
 import EachProfileCard from './components/profile/EachProfileCard';
 import Profile from './components/profile/Profile';
-
+import Nav from './components/nav-bar/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+
       <PurpleCat />
       <RainbowCat />
       <ChatBubble />
--- client/src/components/profile/EachProfileCard.js
+++ client/src/components/profile/EachProfileCard.js
@@ -29,7 +29,6 @@ export default function EachProfileCard() {
           </div>
         </div>
       </div>
-      
     </div>
     
   );
