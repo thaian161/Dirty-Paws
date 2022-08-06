1918cf72759a178030ce41efba90157e03657cf9
Declan Davis <dexthefish@gmail.com>
2022-08-06 17:34:25 -0400

    finish logic for route GET /matches/id/id

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -7,49 +7,63 @@ module.exports = (db) => {
   });
   
   router.get('/:userOne/:userTwo', (req, res) => { // 'Like' another user
-    // change this route to be a 'post' request to './' once it is linked to an axios request,
-    // which should send userOne and userTwo in the req.body. 
-    // Using req.params is a temporary workaround so that we can test in the browser with the url /matches/1/2.
     const {userOne, userTwo} = req.params;
     
-    // queryString selects the match between userOne and userTwo, if it exists 
-    const queryParams = [userOne, userTwo]
+    // find match, if it exists:
+    const queryParams = [userOne, userTwo];
     const queryString = `
-    SELECT * FROM matches 
-    WHERE ( userOne_id = $1 AND userTwo_id = $2 ) 
-    OR ( userOne_id = $2 AND userTwo_id = $1 )`
-    
+      SELECT * FROM matches 
+      WHERE ( userOne_id = $1 AND userTwo_id = $2 ) 
+      OR ( userOne_id = $2 AND userTwo_id = $1 )`;
+
     db.query(queryString, queryParams).then(data => {
-      if (data.rows.length > 0) {
-        // in this case there is a match
-        // this code sets both of its booleans to true
+      if (data.rows.length > 0) { // update the match if one was found:
         const match = data.rows[0];
         const queryParams = [match.id];
         const queryString = `
-        UPDATE matches SET userOne_likes_userTwo = true, userTwo_likes_userOne = true WHERE id = $1`
+          UPDATE matches SET userOne_likes_userTwo = true, userTwo_likes_userOne = true WHERE id = $1 RETURNING *`;
         db.query(queryString, queryParams).then(data => {
-          res.json(match);
+          res.json(['a row has been updated in the matches table', data.rows[0]]);
         })
       }
-      if (data.rows.length == 0) {
-        res.json("a match does not exist")
-        // in this case we should create a match
+      if (data.rows.length == 0) { // create a match if none was found:
+        const queryParams = [userOne, userTwo];
+        const queryString = `
+          INSERT INTO matches (userOne_id, userTwo_id, userOne_likes_userTwo, userTwo_likes_userOne) VALUES ($1, $2, false, true) RETURNING *`;
+        db.query(queryString,queryParams).then(data => {
+          res.json(["a new row has been created in the matches table", data.rows[0]]);
+        })
       }
     })
   });
+
   return router;
 }
 
-// click on the SWIPE RIGHT / LIKE button.
-// Other user has already liked me?
-  // YES: 
-    // get their like
-    // change two_likes_one to true
-    // now we have a true-true match
-    // give the user a message "you have a new match!"
-  // NO:
-    // make a new like
-    // set one_likes_two to true
-    // now we have a true-false match
-    // give the user a message "you have sent a like!" (?)
-// show the user a new profile
+
+// footnote 1:
+  // change this route to a 'post' request to './' once it's linked to an axios request.
+  // Axios.post should send userOne and userTwo in the req.body. 
+  // Using req.params is a temporary workaround so that we can test in the browser with simple GET requests.
+
+// footnote 2:
+  // currently, in 'update the match if one was found', we blindly set both booleans to true.
+  // this means that user A can force a match with user B by 'liking' them twice.
+  // to fix the issue would require a slightly more delicate queryString to target the correct boolean.
+
+// big picture:
+/*
+click on the SWIPE RIGHT / LIKE button.
+Other user has already liked me?
+  YES: 
+    get their like
+    change two_likes_one to true
+    now we have a true-true match
+    give the user a message "you have a new match!"
+  NO:
+    make a new like
+    set one_likes_two to true
+    now we have a true-false match
+    give the user a message "you have sent a like!" (?)
+show the user a new profile
+*/
