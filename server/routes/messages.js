c504459c92f3bad4987e03fa1c3be665de4acd99
DLindeblom <drdlindeblom@gmail.com>
2022-08-06 13:34:25 -0400

    add get and post route for messages in this branch

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,10 +1,26 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
 module.exports = (db) => {
 
   router.get('/', (req, res) => {
-    res.json('made it to /messages');
+
+    const queryString = `SELECT content FROM messages WHERE sender_id = $1 AND receiver_id = $2 OR sender_id = $2 AND receiver_id = $1;`
+    const queryParams = [1, 2]
+
+    db.query(queryString, queryParams).then(data => {
+      res.json(data.rows)
+    })
   })
-  
-  return router;
+
+  router.post('/', (req, res) => {
+
+    const queryString = `INSERT INTO messages (sender_id, receiver_id, content) VALUES ($1, $2, $3)`
+    const queryParams = [1, 2, "They sure are!"]
+
+    db.query(queryString, queryParams).then(data => {
+      res.json(data)
+    })
+  })
+
+return router;
 }
