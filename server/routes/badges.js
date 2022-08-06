add3e8241b2fe60cc2c76fddc9641335c4b490af
DLindeblom <drdlindeblom@gmail.com>
2022-08-06 14:25:19 -0400

    add get route to display user badges

--- server/routes/badges.js
+++ server/routes/badges.js
@@ -3,7 +3,20 @@ const router = require('express').Router();
 module.exports = (db) => {
   
   router.get('/', (req, res) => {
-    res.json('made it to /badges');
+
+    const queryString = `
+    SELECT badges.* 
+    FROM badges 
+    JOIN user_badges ON badges.id = user_badges.badge_id
+    JOIN users ON user_badges.user_id = users.id
+    WHERE users.id = $1
+    `
+    const queryParams = [1]
+
+    db.query(queryString, queryParams).then(data => {
+      res.json(data.rows);
+    })
+    
   });
 
   return router;
