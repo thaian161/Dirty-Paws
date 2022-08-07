376822a8ab308b138194aa56d96e95f4144275dc
Declan Davis <dexthefish@gmail.com>
2022-08-06 21:13:08 -0400

    setup react-router skeleton

--- client/src/components/nav-bar/Nav.js
+++ client/src/components/nav-bar/Nav.js
@@ -2,6 +2,8 @@ import './Nav.css';
 import clearLogo from './images/clearLogo.png';
 import whiteLogo from './images/whiteLogo.png';
 import match from './images/match.png';
+import { Link } from "react-router-dom";
+
 
 export default function Nav() {
   return (
@@ -13,12 +15,21 @@ export default function Nav() {
       </div>
 
       <nav className="navbar">
-       
+        
+        {/* 
         <a className="nav-btn" href="#sign-up">About</a>
         <a className="nav-btn" href="#profile">My Profile</a>
         <a className="nav-btn" href="#log-in">Sign In</a>
         <a className="nav-btn" href="#sign-up">Sign Up</a>
         <a className="nav-btn" href="#sign-up">Search</a>
+        */}
+        
+        <Link className="nav-btn" to="/about"  >About</Link>
+        <Link className="nav-btn" to="/profile">My Profile</Link>
+        <Link className="nav-btn" to="/login">Sign In</Link>
+        <Link className="nav-btn" to="/sign-up">Sign Up</Link>
+        <Link className="nav-btn" to="/search">Search</Link>
+
       </nav>
     </header>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import ExampleComponent from './components/example/Example'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<App />} />
+        <Route path="/profile" element={<App />} />
+        <Route path="/login" element={<App />} />
+        <Route path="/sign-up" element={<App />} />
+        <Route path="/search" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
