616f49e8e3687b67e80043aa5c277fd9ab12c27b
thaian161 <thaian161@yahoo.com>
2022-08-07 04:53:30 -0500

    complete landing page and implement chat bubble

--- client/src/components/landing/LandingPage.js
+++ client/src/components/landing/LandingPage.js
@@ -4,37 +4,38 @@ import HumanCat from '../../lotties/HumanCat';
 // import PurpleCat from '../../lotties/PurpleCat';
 // import Animals from '../../lotties/Animals';
 // import SpaceDog from '../../lotties/SpaceDog';
-import ChatLoveBubble from '../../lotties/ChatLoveBubble';
+// import ChatLoveBubble from '../../lotties/ChatLoveBubble';
 import chatbot from './images/chatbot.png';
 
 export default function LandingPage() {
   return (
     <body>
       <section className="home" id="home">
+        <div className="landing-animation-container">
+          <HumanCat />
+        </div>
+        
         <div className="homeContent">
-          <h3 className="intro-large"> Looking For Your Soulmate? </h3>
-          <p className="intro-small"> Ready To Get Your Paws Dirty</p>
-          <p>
-            I need some dummy content to fill this section for the sake of my
-            design halp halp halp halp halp halp halp halp halp halp halp halp
-            halp halp halp halp halp halp
+          <h2 className="intro-large"> More Than Just A Paws </h2>
+          <h3 className="intro-small"> It's Never Too Late To Fall In Love</h3>
+          <p className="landing-p">
+            Talking to another paws makes being single fun and rewarding. One
+            day, someone will walk into your life and make you see why it never
+            worked out with anyone else. Finding a paws is a hug highlight, but
+            with Dirty Paws...you are swiping right on this one.
+            <br />
+            Dirty Paws - more than just a dating app. Find Your Paws Today
           </p>
           <a href="/#">
-            <button className="land-btn">explore more</button>
-          </a>
-        </div>
-
-        <div className="landing-animation-container">
-          <div>
-            <HumanCat />
-          </div>
-        </div>
-        <div className="chat-box">
-          <a href="/" className="logo-chatbot">
-            <img src={chatbot} alt="" />
+            <button className="land-btn">Explore More</button>
           </a>
         </div>
       </section>
+      <div className="chat-box">
+        <a href="/" className="logo-chatbot">
+          <img src={chatbot} alt="" />
+        </a>
+      </div>
     </body>
   );
 }
--- client/src/lotties/HumanCat.jsx
+++ client/src/lotties/HumanCat.jsx
@@ -3,8 +3,8 @@ import humanCat from './human-cat.json';
 
 const HumanCat = () => {
   const defaultOptions = {
-    loop: false,
-    autoplay: false,
+    loop: true,
+    autoplay: true,
     animationData: humanCat,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
@@ -13,7 +13,7 @@ const HumanCat = () => {
 
   return (
     <div>
-      <Lottie options={defaultOptions} height={700} width={700} />
+      <Lottie options={defaultOptions} height={520} width={520} />
     </div>
   );
 };
