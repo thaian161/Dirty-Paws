5ba2bf4f04595ebde5f1f32db3ea0b4235fd34e7
Declan Davis <dexthefish@gmail.com>
2022-08-07 20:38:03 -0400

    add basic react-router skeleton with links in nav

--- front-end/src/components/nav-bar/Nav.js
+++ front-end/src/components/nav-bar/Nav.js
@@ -2,6 +2,7 @@ import './Nav.css';
 import clearLogo from './images/clearLogo.png';
 import whiteLogo from './images/whiteLogo.png';
 import match from './images/match.png';
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
@@ -14,11 +15,14 @@ export default function Nav() {
 
       <nav className="navbar">
        
-        <a className="nav-btn" href="#sign-up">About</a>
+        {/* <a className="nav-btn" href="#sign-up">About</a>
         <a className="nav-btn" href="#profile">My Profile</a>
         <a className="nav-btn" href="#log-in">Sign In</a>
         <a className="nav-btn" href="#sign-up">Sign Up</a>
-        <a className="nav-btn" href="#sign-up">Search</a>
+        <a className="nav-btn" href="#sign-up">Search</a> */}
+        <Link className="nav-btn" to="/">About</Link>
+        <Link className="nav-btn" to="/home">Landing Page</Link>
+        <Link className="nav-btn" to="/profile">My Profile</Link>
       </nav>
     </header>
   );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPage from './components/landing/LandingPage';
+import Profile from './components/profile/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<LandingPage/>} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<App />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
