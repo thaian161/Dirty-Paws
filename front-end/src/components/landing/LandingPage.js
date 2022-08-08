b5284d57c46ea33607efd5a947a3bc80bafd41cd
thaian161 <thaian161@yahoo.com>
2022-08-08 06:15:23 -0500

    refactor landing page

--- front-end/src/components/landing/LandingPage.js
+++ front-end/src/components/landing/LandingPage.js
@@ -1,34 +1,32 @@
 import './LandingPage.css';
-import HumanCat from '../../lotties/HumanCat';
-// import ChatBubble from '../../lotties/ChatBubble';
-// import PurpleCat from '../../lotties/PurpleCat';
-// import Animals from '../../lotties/Animals';
-// import SpaceDog from '../../lotties/SpaceDog';
-// import ChatLoveBubble from '../../lotties/ChatLoveBubble';
 import chatbot from './images/chatbot.png';
+import petsPink from './images/petsPink.png';
 
 export default function LandingPage() {
   return (
     <body>
       <section className="home" id="home">
-        <div className="landing-animation-container">
-          <HumanCat />
-        </div>
+       
         
         <div className="homeContent">
           <h2 className="intro-large"> More Than Just A Paws </h2>
           <h3 className="intro-small"> It's Never Too Late To Fall In Love</h3>
           <p className="landing-p">
-            Talking to another paws makes being single fun and rewarding. One
+            One
             day, someone will walk into your life and make you see why it never
             worked out with anyone else. Finding a paws is a hug highlight, but
             with Dirty Paws...you are swiping right on this one.
-            <br />
+           
             Dirty Paws - more than just a dating app. Find Your Paws Today
           </p>
+          <br/>
           <a href="/#">
             <button className="land-btn">Explore More</button>
           </a>
+        </div>
+
+        <div className="landing-animation-container">
+          <img className="landing-pic" src={petsPink} alt=''/>
         </div>
       </section>
       <div className="chat-box">
