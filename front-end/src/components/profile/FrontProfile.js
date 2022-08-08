e1579663786b8aa38653393d46cded50e10137c0
thaian161 <thaian161@yahoo.com>
2022-08-08 00:18:38 -0500

    complete FRONT CARD

--- front-end/src/components/profile/FrontProfile.js
+++ front-end/src/components/profile/FrontProfile.js
@@ -6,37 +6,39 @@ export default function FrontProfile() {
   return (
     <div className="container">
       <div className="wrapper">
-        <img
-          className="front-profile-pic"
-          src="https://img.freepik.com/premium-vector/cute-welsh-corgi-dog-waving-paw-cartoon_42750-623.jpg"
-          alt="front profile card"
-        />
-        <div className="content">
-          <h2 className="user-name"> Mr. Sleepy Head </h2>
-          <div className="profile-footer">
-            <div>
-              <p className="profile-title">AGE</p>
-              <p className="num">19</p>
-            </div>
-            <div>
-              <p className="profile-title">CITY</p>
-              <p className="num">Orlando</p>
-            </div>
-            <div>
-              <p className="profile-title">TREATS</p>
-              <p className="num">1000</p>
-            </div>
-          </div>
-          <div className="love-me-not">
-            <button>
-              <img className="match-btn" src={noSoSad} alt="no love me" />
-            </button>
-            <button>
-              <img className="match-btn" src={yes} alt="yes love me" />
-            </button>
-          </div>
-          <div className="empty-bottom">......</div>
+        <div className="profile-top">
+          <img
+            className="front-profile-pic"
+            src="https://img.freepik.com/premium-vector/cute-welsh-corgi-dog-waving-paw-cartoon_42750-623.jpg"
+            alt="front profile card"
+          />
         </div>
+        <div className="content">
+          <h2 className="user-name"> Sleepy Head </h2>
+        </div>
+        <div className="profile-footer">
+          <div>
+            <p className="profile-title">AGE</p>
+            <p className="num">19</p>
+          </div>
+          <div className="city">
+            <p className="profile-title">CITY</p>
+            <p className="num">Orlando</p>
+          </div>
+          <div>
+            <p className="profile-title">TREATS</p>
+            <p className="num">77</p>
+          </div>
+        </div>
+        <div className="love-me-not">
+          <button>
+            <img className="sad-btn" src={noSoSad} alt="no love me" />
+          </button>
+          <button>
+            <img className="love-btn" src={yes} alt="yes love me" />
+          </button>
+        </div>
+        <div className="empty-bottom">......</div>
       </div>
     </div>
   );
