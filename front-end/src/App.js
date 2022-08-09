a4750cfb16bf66e6ba730c6671b3410932f6ccd5
thaian161 <thaian161@yahoo.com>
2022-08-08 20:11:36 -0500

    chatListleft

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,7 +12,11 @@ import Nav from './components/nav-bar/Nav';
 import LandingPage from './components/landing/LandingPage';
 import BackProfile from './components/profile/BackProfile';
 import MatchProfile from './components/profile/MatchProfile';
-import ChatList from './components/chat-page/chatContent/ChatList';
+import ChatList from './components/chat-page/chatListLeft/ChatList';
+import ChatContentItem from './components/chat-page/chatContent/ChatContentItem';
+import ChatContent from './components/chat-page/chatContent/ChatContent';
+import ChatBody from './components/chat-page/chatBody/ChatBody';
+import Chat from './components/chat-page/Chat';
 
 function App() {
   return (
@@ -31,6 +35,12 @@ function App() {
       <Profile />
 
       <ChatList />
+
+      <ChatContentItem />
+      <ChatContent/>
+      <ChatBody/>
+
+      <Chat/> 
     </div>
   );
 }
---
+++ front-end/src/components/chat-page/Chat.js
@@ -0,0 +1,16 @@
+
+import "./Chat.css";
+import ChatBody from "./chatBody/ChatBody";
+
+
+function Chat() {
+  return (
+    <div className="__main">
+
+<ChatBody/>
+     
+    </div>
+  );
+}
+
+export default Chat;
