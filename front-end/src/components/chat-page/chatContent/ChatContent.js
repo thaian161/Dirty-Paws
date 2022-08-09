959fa78c3643e2726741ca9ac23836784bfb0d9a
Declan Davis <dexthefish@gmail.com>
2022-08-09 00:51:58 -0400

    resolve import statements

--- front-end/src/components/chat-page/chatContent/ChatContent.js
+++ front-end/src/components/chat-page/chatContent/ChatContent.js
@@ -1,5 +1,5 @@
-import './chatContent.css';
 import Avatar from '../chatListLeft/Avatar';
+import './chatContent.css';
 import ChatContentItem from './ChatContentItem';
 
 export default function ChatContent() {
@@ -8,8 +8,11 @@ export default function ChatContent() {
       <div className="content__header">
         <div className="blocks">
           <div className="current-chatting-user">
-            <Avatar image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU" />
-            <p>Sexi Boi</p>
+            <Avatar />
+            <p> Sexi Boiiii</p>
+
+
+            
           </div>
         </div>
       </div>
@@ -17,30 +20,12 @@ export default function ChatContent() {
         <div className="chat__items">
           <ChatContentItem />
         </div>
-        <div className="chat__items">
-          <ChatContentItem />
-        </div>
-        <div className="chat__items">
-          <ChatContentItem />
-        </div>
-        <div className="chat__items">
-          <ChatContentItem />
-        </div>
       </div>
       <div className="content__footer">
         <div className="sendNewMessage">
-          <input
-            type="text"
-            placeholder="Type a message here"
-            // INPUT also go with onChange and Value
-            // onChange={}
-            // value={}
-          />
+          <input type="text" placeholder="Type a message here" />
           <button className="btnSendMsg" id="sendMsgBtn">
-            <img className="mes-icon"
-              src="https://cdn-icons-png.flaticon.com/512/8016/8016417.png"
-              alt="send message icon"
-            />
+            ✈
           </button>
         </div>
       </div>
