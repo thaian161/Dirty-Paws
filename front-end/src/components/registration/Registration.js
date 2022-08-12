3bfe651c5668fa607af039d39e555dc336794564
thaian161 <thaian161@yahoo.com>
2022-08-11 19:56:38 -0500

    add Front and Back to Register

--- front-end/src/components/registration/Registration.js
+++ front-end/src/components/registration/Registration.js
@@ -1,63 +1,62 @@
 import './registration.css';
+import FrontProfile from '../../components/profile/FrontProfile';
+import BackProfile from '../../components/profile/BackProfile';
 
 export default function Registration() {
   return (
-    <section className="home-land" id="homee">
-      <div className="landing-animation-container">
-        <img
-          className="landing-pic"
-          src="https://i.pinimg.com/originals/89/ec/4e/89ec4e1d540d5b04bb5808632413be88.jpg"
-          alt=""
-        />
+    <section className="register-section">
+      <div className="register-form">
+        <h2 className="intro-large"> Create Your Profile </h2>
+        <form action="/" method="POST">
+          <div>
+            <h3 className="intro-small">
+              To start pawsing, fill out the form below:
+            </h3>
+          </div>
+          <div>
+            {/* <!-- NAME --> */}
+            <div>
+              <label className="landing-p">Name</label>
+              <input class="form-control sign-up" required />
+            </div>
+
+            {/* <!-- AGE --> */}
+            <div class="form-group">
+              <label className="landing-p">Age</label>
+              <input class="form-control sign-up" required />
+            </div>
+
+            {/* <!-- CITY --> */}
+            <div class="form-group">
+              <label className="landing-p">City</label>
+              <input class="form-control sign-up" required />
+            </div>
+
+            {/* <!-- BIO --> */}
+            <div class="form-group">
+              <label className="landing-p">Bio</label>
+              <input class="form-control sign-up" required />
+            </div>
+
+            {/* <!-- AVATAR --> */}
+            <div class="form-group">
+              <label className="landing-p">Avatar</label>
+              <input class="form-control sign-up" required />
+            </div>
+          </div>
+
+          {/* <!-- SUBMIT BUTTON --> */}
+
+          <button type="submit" className="land-btn signup-btn ">
+            Let's Paws
+          </button>
+        </form>
       </div>
-      <div className="homeContent">
-        <div>
-          <h2 className="intro-large"> Create Your Profile </h2>
-          <form action="/" method="POST">
-            <div>
-              <h3 className="intro-small">
-                To start pawsing, fill out the form below:
-              </h3>
-            </div>
-            <div>
-              {/* <!-- NAME --> */}
-              <div>
-                <label className="landing-p">Name</label>
-                <input class="form-control sign-up" required />
-              </div>
-
-              {/* <!-- AGE --> */}
-              <div class="form-group">
-                <label className="landing-p">Age</label>
-                <input class="form-control sign-up" required />
-              </div>
-
-              {/* <!-- CITY --> */}
-              <div class="form-group">
-                <label className="landing-p">City</label>
-                <input class="form-control sign-up" required />
-              </div>
-
-              {/* <!-- BIO --> */}
-              <div class="form-group">
-                <label className="landing-p">Bio</label>
-                <input class="form-control sign-up" required />
-              </div>
-
-              {/* <!-- AVATAR --> */}
-              <div class="form-group">
-                <label className="landing-p">Avatar</label>
-                <input class="form-control sign-up" required />
-              </div>
-            </div>
-
-            {/* <!-- SUBMIT BUTTON --> */}
-
-            <button type="submit" className="land-btn signup-btn ">
-              Let's Paws
-            </button>
-          </form>
-        </div>
+      <div>
+        <FrontProfile />
+      </div>
+      <div>
+        <BackProfile />
       </div>
     </section>
   );
